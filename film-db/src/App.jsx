b345a7ebdea35289c81d7ceb342824d500ebf975
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-10-06 17:16:51 +0200

    created component

--- film-db/src/App.jsx
+++ film-db/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import getData from './Hooks/useContentful';
+// import { useState } from 'react';
+import "./App.css";
+import NavBar from "./components/NavBar";
+// import getData from './Hooks/useContentful';
 
 function App() {
-  const [count, setCount] = useState(0)
-  getData()
+  // const [count, setCount] = useState(0)
+  // getData()
   return (
-    <div className=''>
-      <NavBar />    
+    <div className="">
+      <NavBar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
