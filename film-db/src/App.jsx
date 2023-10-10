9c961ad7bfb15dc780ab9c96ad139fd3de45a647
RockandRolls <livelydev010@gmail.com>
2023-10-10 15:03:37 +0200

    added Comedy Carousel

--- film-db/src/App.jsx
+++ film-db/src/App.jsx
@@ -3,75 +3,41 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Trending from './components/Trending';
 // import SingleFilmPg3 from './components/SingleFilmPg3 ';
-
 import  client  from './lib/contentfulClient';
-// FilmCard is collection of all films
+// FilmCard is collection of all films in contentful
 import 'react-multi-carousel/lib/styles.css';
 
 
 
 const App = () => {
-  // const [CarouselLoading, setCarouselLoading]= useState(false)
-  const [films, setFilms] = useState([])
-  // null or array?
-
-  // if useCallback function is not used, u get warnings in code
-  const getSlides =async () => {
-    try {
-      const response = await client.getEntries({
-        content_type: 'filmCard',
-      });
-
-      // .getEntries() is a method to get all entries
-  
-      // content-type is id of filmCard---u find copy id beside it in contentful
-      const responseData = response.items;
-      
-      setFilms(responseData)
-      // in browser console when u expand array, u find items which u need to get
-      console.log(responseData);
-      // u will not see console from browser. u need use useEffect. 
-      return response;
-    } catch (error) {
-      console.error(error.message);
-    }
+    // const [CarouselLoading, setCarouselLoading]= useState(false)
+    const [films, setFilms] = useState([])
+    // null or array?
+    const getSlides =async () => {
+        try {
+          const response = await client.getEntries({
+            content_type: 'filmCard',
+          });
+          // .getEntries() is a method to get all entries
+          // content_type is id of filmCard---u find copy id beside it in contentful
+          const responseData = response.items;
+          setFilms(responseData)
+          // in browser console when u expand array, u find items which u need to get
+          // u will not see console from browser. u need use useEffect. 
+          return response;
+        } catch (error) {
+          console.error(error.message);
+        }
   }
 
-  useEffect(() => {
-    getSlides()
+  useEffect(() => {getSlides()
   }, [])
   
-
-//   useEffect(() => {
-//     const fetchData= async () => {
-//     try{
-//     const getFilmData= await fetch(
-//       'https://cdn.contentful.com/spaces/gwp51gar4wfb/environments/master/entries?access_token=em2LoVg8vTVQJlAIgQpARAigO4zS715njiG3S_jLwTA'
-//     )
-//       if (!getFilmData.ok)
-//       throw new Error(
-//           'The request failed with a status of '+ getFilmData.status
-//         );
-//         const parsedFilms = await getFilmData.json();
-
-//         console.log (parsedFilms);
-
-//         setAllFilms(parsedFilms);
-
-//     }catch((error) => console.error(error));
-//   }, []);
-
-
-// }
-
-
-  return (
+ return (
     <div className=''>
       <NavBar /> 
-
       <Trending  films={films}/> 
       {/* <SingleFilmPg3/> */}
-
     </div>
   )
 }
--- film-db/src/lib/contentfulClient.js
+++ film-db/src/lib/contentfulClient.js
@@ -1,20 +1,10 @@
 import  { createClient } from 'contentful';
 
-
 const client = createClient({
   // hid or code in .env.local file which is on same level as package.json
-  
-  space: import.meta.env.VITE_SPACE_ID,
-  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+    space: import.meta.env.VITE_SPACE_ID,
+    accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 })
-
-// export default async function getData() { 
-//     const entries = await client.getEntries();
-//     console.log(entries)
-// }
-
-
-// export { getSlides };
 
 export default client;
 
