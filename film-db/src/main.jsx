2e2192884dcec8deba9d76b972b36fa1cd0264ca
Catia <wolfgangcatia@gmail.com>
2023-10-11 14:57:23 +0200

    no major changes

--- film-db/src/main.jsx
+++ film-db/src/main.jsx
@@ -1,13 +1,10 @@
-
-import ReactDOM from "react-dom/client";
 import {createRoot} from 'react-dom/client';
 import App from "./App.jsx";
 import "./index.css";
 import "./styles.scss";
 import { NextUIProvider } from "@nextui-org/react";
 
-ReactDom.createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById('root')).render(
   <NextUIProvider>
     <main className="light text-foreground bg-background">
       <App />
