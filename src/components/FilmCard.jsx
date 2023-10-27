833eea1c8064e080645203acd7e720f8ec3dd43a
RockandRolls <livelydev010@gmail.com>
2023-10-27 13:00:46 +0200

    changed names

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -8,7 +8,7 @@ const FilmCard = ({ film }) => {
   const {filmname, imagesrc, year}= film;
   // blue underlines r just spell check not error
   
-  // console.log(filmname,imagesrc,year, genre);
+  console.log(filmname, imagesrc, year);
   
   // console.log(filmName,imageSrc,year, genre);
   // const imgStyle= {
