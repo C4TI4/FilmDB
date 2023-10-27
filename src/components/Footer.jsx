d47b913858428bd647a8d5d8d59cd420dc6e7191
Catia <wolfgangcatia@gmail.com>
2023-10-27 10:46:04 +0200

    minor styling changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <>
         <div className='p-8 text-center bg-black text-white'>
-          <h2 className='text-3xl p-7 font-bold text-red-700'>More Movies at cheaper rates !!!</h2>         
+          <h2 className='text-3xl p-7 font-bold text-red-700'>Want more?</h2>         
           <ul className='text-white flex gap-6 pr-10 justify-center text-lg font-semibold'>
               <li>
                 <button onClick={handleClick}>Trending</button>
